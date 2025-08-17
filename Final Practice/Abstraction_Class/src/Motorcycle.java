89025b382a86685e958ece436683a6d1afebc324
Hossain Ahmmed <hossainahmmedtaufiq22@gmail.com>
2025-08-17 15:47:16 +0600

    Lab manual done

---
+++ Final Practice/Abstraction_Class/src/Motorcycle.java
@@ -0,0 +1,5 @@
+public class Motorcycle extends Vehicle {
+    void start() {
+        System.out.println("Motorcycle start");
+    }
+}
---
+++ Final Practice/FileException/src/Main.java
@@ -0,0 +1,34 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.FileReader;
+import java.io.FileWriter;
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+        String fileName= "test.txt";
+
+
+        try(BufferedWriter writer = new BufferedWriter(new FileWriter(fileName))){
+            writer.write("Hello From test");
+            System.out.println("File write successfully");
+        }catch (Exception e){
+            System.out.println(e);
+        }
+
+
+
+//        reading
+        try(BufferedReader reader = new BufferedReader(new FileReader(fileName))){
+            String line;
+
+            while((line=reader.readLine())!=null){
+                System.out.println(line);
+            }
+            System.out.println("File read successfully");
+        }catch (Exception e){
+            System.out.println(e);
+        }
+    }
+}
